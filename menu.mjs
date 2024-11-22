1f54cedc9ec89741ce94a2a09992415e488e421e
Thalles <thallesttt.11@gmail.com>
2024-11-22 20:37:53 -0300

    inclusão do pulumi e ajuste de teste. alteração para contrução de arquivos .mjs

---
+++ menu.mjs
@@ -0,0 +1,17 @@
+/**Disponibilização de um menu de lanches
+ * @param {string} tituloCatalogo título do catálogo necessário para visualização do cliente
+ * */
+function geraMenu(tituloCatalogo, itens) {   
+    let htmlDisplay = "";
+    if(tituloCatalogo) {
+        htmlDisplay = `<ol name="${tituloCatalogo}">`;
+        for(let iterator = 0; iterator < itens.length; iterator++) {
+            const {Name, Price} = itens[iterator];
+            let item = `<li value="" class=""><span>${Name} &nbsp; - R$${Price.replace(".",",")}</span></li>`;
+            htmlDisplay += item;    
+        }
+        htmlDisplay += "</ol>";
+    }
+    return htmlDisplay;
+}
+module.exports = geraMenu;
--- menu.test.js
+++ menu.test.js
@@ -1,5 +1,6 @@
-const fctn = require("./menu.js");
+const fctn = require("./menu.mjs");
 test("Disponibilização de um menu de lanches", () => {
     let object = {name:"Lanches", itens:[{Id:1,Name:"Pão na chapa", Price:"2.20"}]};
-expect(fctn(object.name, object.itens)).toEqual("<ol name='Lanches'><li class=''><span>Pão na chapa &nbsp; - R$2,20</span></li></ol>");
+    let result = fctn(object.name, object.itens);
+expect(result).toEqual(`<ol name="Lanches"><li value="" class=""><span>Pão na chapa &nbsp; - R$2,20</span></li></ol>`);
 });
